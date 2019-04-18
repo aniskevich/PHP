56f9c1eea735c9ceb0e5bb63c676e61a10fbeb3b
Boris Aniskevich <aniskevich.bo@gmail.com>
2019-04-18 13:19:36 +1000

    HW1

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+        $header = '<h1>Заголовок</h1>';
+        $title = 'Document';
+        $year = getdate();
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title><?php echo $title ?></title>
 </head>
 <body>
-    
+    <?php   
+        echo $header;
+        echo $year[year];
+            
+        // 2* задание
+        $a = 1;
+        $b = 2;
+        [$a, $b] = [$b, $a];
+    ?>   
 </body>
 </html>
